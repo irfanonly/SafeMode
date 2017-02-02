edf1ad1a296c5df18aaad6469fffa1b00ba62d58
Irfan <mamirfan92@gmail.com>
2017-02-02 22:42:03 +0530

    add course

--- SafeMode/Controllers/ManageCertController.cs
+++ SafeMode/Controllers/ManageCertController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PagedList;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,8 +10,32 @@ namespace SafeMode.Controllers
     [Authorize(Roles = "admin")]
     public class ManageCertController : Controller
     {
+        safeModeEntities db = new safeModeEntities();
         // GET: ManageCert
-        public ActionResult Index()
+        public ActionResult Index(string name, int page = 1)
+        {
+            if (TempData["Succuss"] != null)
+            {
+                ViewBag.SuccessMsg = TempData["Succuss"];
+            }
+            if (TempData["Error"] != null)
+            {
+                ViewBag.ErrorMsg = TempData["Error"];
+            }
+
+            var certs = db.Certificates.OrderByDescending(x => x.uploadedon);
+
+            if (name != null && name != "")
+            {
+                ViewBag.name = name;
+                certs = certs.Where(x => x.name.Contains(name)).OrderBy(x => x.name);
+            }
+
+            return View(certs.ToPagedList(page, 10));
+        }
+
+
+        public ActionResult Add()
         {
             return View();
         }
