db4ed2dc8e0c48c1996f3cada242eef148dd9348
Irfan <mamirfan92@gmail.com>
2017-04-02 21:56:50 +0530

    profile view

--- SafeMode/Controllers/AdminController.cs
+++ SafeMode/Controllers/AdminController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SafeMode.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -18,6 +19,17 @@ namespace SafeMode.Controllers
         public ActionResult Profile()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Profile(ProfileVM model)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+
+            return View(model);
         }
     }
 }
