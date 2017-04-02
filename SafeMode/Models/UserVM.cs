0a7a13ef8c83c7db3fef90d9f964a6569e8179f5
Irfan <mamirfan92@gmail.com>
2017-04-02 18:31:14 +0530

    my profile

--- SafeMode/Models/UserVM.cs
+++ SafeMode/Models/UserVM.cs
@@ -58,6 +58,33 @@ namespace SafeMode.Models
         public string ConfirmPassword { get; set; }
     }
 
+    public class ProfileVM
+    {
+
+        public string Id { get; set; }
+
+        [Required]
+        [Display(Name = "Username")]
+        public string UserName { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string CurrentPassword { get; set; }
+
+        public bool ChangePassword { get; set; }
+       
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
 
     public class UserEditVM
     {
